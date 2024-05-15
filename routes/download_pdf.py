02ab7cdd17ae7915ed2a26fca2e125e3a8639919
Lahiru <lahirupraveen43@gmail.com>
2024-05-16 02:27:47 +0530

    Done most of stylies in db and cp

--- routes/download_pdf.py
+++ routes/download_pdf.py
@@ -1,15 +1,24 @@
-# import tempfile
-# from weasyprint import HTML
-# from fastapi import APIRouter
+from fastapi import APIRouter
+from fastapi import Response, Request
+from weasyprint import HTML
 
-# down_pdf_router = APIRouter()
+down_pdf_router = APIRouter()
 
-# @down_pdf_router.post("/generate-pdf")
-# async def generate_pdf(review_content: str):
-#     try:
-#         html_content = f"<html><body>{review_content}</body></html>"
-#         pdf_file = tempfile.NamedTemporaryFile(delete=False)
-#         HTML(string=html_content).write_pdf(pdf_file.name)
-#         return FileResponse(pdf_file.name, media_type='application/pdf')
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+
+@down_pdf_router.post('/generate-pdf')
+async def generate_pdf(request: Request, review_content: str):
+    print("Request Headers:", request.headers)
+    print("Request Body:", await request.body())
+
+    if not review_content:
+        return {"error": "review_content is required"}
+
+    try:
+        html_content = f"<html><body>{review_content}</body></html>"
+        pdf_file = HTML(string=html_content).write_pdf()
+        headers = {
+            'Content-Disposition': 'attachment; filename="review_content.pdf"'
+        }
+        return Response(pdf_file, headers=headers, media_type='application/pdf')
+    except Exception as e:
+        return {'error': str(e)}
