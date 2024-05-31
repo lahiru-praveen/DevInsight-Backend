369f56686844d9433c65e4f6052760ae47e439b2
Lahiru <lahirupraveen43@gmail.com>
2024-05-31 17:56:14 +0530

    Review has 3 parts it will generate using 3 prompts

--- routes/interact_llm.py
+++ routes/interact_llm.py
@@ -1,10 +1,9 @@
 from fastapi import HTTPException, APIRouter
-
 from database.db import DatabaseConnector
 from models import code_context_data, code_review
 import textwrap
-from utilis.generate_response import CodeReviewLLM
 import logging
+from utilis.llm.optimizer import CodeReviewLLM1
 
 CHUNK_SIZE = 20000  # Adjust based on transcript length and model limits
 db1 = DatabaseConnector("code")
@@ -27,41 +26,16 @@ async def get_review(code_context: code_context_data.CodeContextData):  # Receiv
         chunks = textwrap.wrap(file_content, width=CHUNK_SIZE)
         input_chunks = chunks
 
-        result = CodeReviewLLM.get_review(input_chunks, code_context.language, code_context.description)
+        review,suggestions,refer_link = CodeReviewLLM1.get_review(input_chunks, code_context.language, code_context.description)
 
-        if not result:
-            raise HTTPException(status_code=500, detail="LLM test failed or returned empty result")
+        if not review:
+            raise HTTPException(status_code=500, detail="LLM test failed or returned empty review")
 
-        print(result)
-        return result
+        print(review)
+        print(suggestions)
+        print(refer_link)
 
-    except HTTPException as http_err:
-        logging.error(f"HTTP error occurred: {http_err}")
-        raise http_err
-
-    except Exception as e:
-        logging.error(f"Error in get_code: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-
-@llm_router.post("/get_suggestions")
-async def get_suggestions(review_context: code_review.CodeReviewData):  # Receive codeContext from the request body
-    try:
-        review_content = review_context.review
-        code_content = review_context.code
-
-        if not review_content and not code_content:
-            raise HTTPException(status_code=400, detail="Review, Code content is empty or not provided")
-
-        chunks = textwrap.wrap(code_content, width=CHUNK_SIZE)
-        input_chunks = chunks
-
-        result = CodeReviewLLM.get_suggestions(input_chunks, review_context.review)
-
-        if not result:
-            raise HTTPException(status_code=500, detail="LLM test failed or returned empty result")
-
-        print(result)
-        return result
+        return {"review": review, "suggestions": suggestions, "refer_link": refer_link}
 
     except HTTPException as http_err:
         logging.error(f"HTTP error occurred: {http_err}")
